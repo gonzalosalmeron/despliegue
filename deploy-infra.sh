eff5bfed8a46009ec04e41980b8da97c755d5a29
Gonzalo Salmerón <gonzxlosalmeron@gmail.com>
2023-02-07 12:18:02 +0100

    added setup.yml and screenshots

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -2,8 +2,21 @@
 STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=default
-
 EC2_INSTANCE_TYPE=t2.micro
+
+# Variables para el bucket
+AWS_ID=`aws sts get-caller-identity --query "Account" --output text`
+S3_BUCKET_NAME="${REGION}"-pipeline-"${AWS_ID}"
+
+echo -e "\n=========== Desplegando setup.yml (S3 Bucket) =============="
+
+aws cloudformation deploy \
+	--region $REGION \
+	--profile $CLI_PROFILE \
+	--stack-name bucket \
+	--template-file setup.yml \
+	--no-fail-on-empty-changeset \
+	--parameter-override BucketName=$S3_BUCKET_NAME
 
 echo -e "\n=========== Desplegando  main.yml ================="
 
